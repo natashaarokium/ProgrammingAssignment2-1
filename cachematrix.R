e3ccfbb44d5db25f9ce04ae2878e6d16d952b630
natashaarokium <nnarokium@gmail.com>
2016-08-06 23:28:38 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My function was written to create the inverse of a special matrix. This special matrix would be stored in cache.
+## I first initiallized the objects, assigned set to the function (y), then assigned get to the function (x). Assigned 
+## Setinverse to find the inverse of a matrix. Each function is assigned within a list(), which returns it 
+## to the parent environment.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+ m <- NULL
+   set <- function(y) {
+     x <<- y
+      m <<- NULL
+   }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+   list(set = set, get = get,
+         setinverse = setinverse,
+          getinverse = getinverse)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## With cachesolve, codes are able to access the values of both x and m via get and set. Cachesolve is able to find and 
+## store the inverse of a matrix for the input argument makeCacheMatrix. The functions can be accessed by $, because the list 
+## elements in MakeCacheMatrix are defined with names.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+    data <- x$get()
+     m <- solve(data, ...)
+     x$setinverse(m)
+     m
 }
